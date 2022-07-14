7a44259d380a4bed9f7c1174f44107f1fa9a3f6e
Elena Timofeeva <posazho@gmail.com>
2022-07-14 20:45:57 +0300

    создание таблицы.

---
+++ Task48/Program.cs
@@ -0,0 +1,34 @@
+﻿/*  Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5  */
+
+int[,] fillArray(int rowsCount, int columnCount)
+{
+    int[,] result_array = new int[rowsCount,columnCount];
+    for (int i = 0; i < rowsCount; i++)
+    {
+        for (int j = 0; j < columnCount; j++)
+        {
+            result_array[i,j] = i + j;
+        }
+    }
+    return result_array;
+}
+
+void printArray(int[,] arrayForPrint)
+{
+    for (int i = 0; i < arrayForPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayForPrint.GetLength(1); j++)
+        {
+            Console.Write($"{arrayForPrint[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = fillArray(3,4);
+printArray(array);
