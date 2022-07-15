14f7d158cc6c6991d995212841722668c26d7cee
Elena Timofeeva <posazho@gmail.com>
2022-07-15 16:59:46 +0300

    нахождение элемента по позиции

---
+++ Homework2/Program.cs
@@ -0,0 +1,75 @@
+﻿/* Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет */
+
+int[,] Fill2DArray(int rows, int columns)
+{
+    int[,] array = new int[rows,columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(0,10);
+        }   
+    }
+    return array;
+}
+
+void Print2DArray(int[,] arrayForPrint)
+{
+    Console.Write("\t");
+    for (int i = 0; i < arrayForPrint.GetLength(1); i++)
+    {
+        Console.Write($"{i+1}\t");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+    for (int i = 0; i < arrayForPrint.GetLength(0); i++)
+    {
+         Console.Write($"{i+1}\t");        
+        for (int j = 0; j < arrayForPrint.GetLength(1); j++)
+        {            
+            Console.Write(arrayForPrint[i,j]);
+            if (j < arrayForPrint.GetLength(1) - 1)
+            {
+                Console.Write("\t");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] ArrayOfPositions(int[,] array)
+{
+    int length = array.GetLength(0) * array.GetLength(1);
+    int[] arrayOfPosition = new int[length];
+    int index = 0;   
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {               
+                arrayOfPosition[index] = array[i,j];
+                index ++;
+             }
+         }
+    return arrayOfPosition;
+}
+
+
+int[,] ArrayForTask = Fill2DArray(3,4);
+Print2DArray(ArrayForTask);
+int[] arrayOfPositions2D = ArrayOfPositions(ArrayForTask);
+Console.WriteLine("введите номер позиции интересующего Вас элемента: ");
+int element = Convert.ToInt32(Console.ReadLine());
+if (element >= 0 && element < arrayOfPositions2D.Length)
+{
+    Console.WriteLine(arrayOfPositions2D[element]);
+}
+else
+{
+    Console.WriteLine("Элемента с такой позицией не существует в данном массиве.");
+} 
